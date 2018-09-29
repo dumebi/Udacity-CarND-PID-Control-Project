574f795ba7f54553e5b4a9397019d68861f1002f
Jude Dike <dikejude49@gmail.com>
2018-09-29 22:03:50 +0100

    perfect init

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,28 @@
 #include "PID.h"
+#include <algorithm>
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  PID::d_error = (cte - p_error);
+
+  PID::p_error = cte;
+
+  PID::i_error += cte;
 }
 
 double PID::TotalError() {
+  return -PID::Kp * PID::p_error - PID::Kd * PID::d_error - PID::Ki * PID::i_error;
 }
 
